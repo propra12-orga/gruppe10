1081091c95f3e2281ddbb9fc672ca12c8728cc84
Chris <christopher.vogt@uni-duesseldorf.de>
2012-05-10 03:37:02 +0200

    spielfeld ohne alles

---
+++ src/SPIELFELD.java
@@ -0,0 +1,7 @@
+public class SPIELFELD {
+	public static void main(String[] args) {
+		StdDraw.setXscale(0.0, 1000.0);	
+		StdDraw.setYscale(0.0, 1000.0);
+		StdDraw.filledSquare(500.0, 500.0, 500.0);
+	}
+}
