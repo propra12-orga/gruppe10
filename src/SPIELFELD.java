c24afacd9e58e3b4711af06127b4ef6a48ecf621
Nils <Nils@Nils-PC.(none)>
2012-05-14 15:27:08 +0200

    Verbessertes Spielfed

--- src/SPIELFELD.java
+++ src/SPIELFELD.java
@@ -2,6 +2,6 @@ public class SPIELFELD {
 	public static void main(String[] args) {
 		StdDraw.setXscale(0.0, 1000.0);	
 		StdDraw.setYscale(0.0, 1000.0);
-		StdDraw.filledSquare(500.0, 500.0, 500.0);
+		StdDraw.square(500.0, 500.0, 500.0);
 	}
 }
