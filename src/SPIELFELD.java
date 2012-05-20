40f3d8d7e677b0eddc07798bd3e0bfebc7267109
Christopher Vogt <christopher.vogt@uni-duesseldorf.de>
2012-05-20 22:52:55 +0200

    neues, sauberes Spielfeld mit Array, Gridlayout und Kommentaren

--- src/SPIELFELD.java
+++ src/SPIELFELD.java
@@ -1,20 +1,23 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
+import java.awt.GridLayout;
+import javax.swing.*;
 
 public class SPIELFELD extends JFrame{
-	public static void main(String[] args) {
-		new SPIELFELD();
-	}		
-		
-	public SPIELFELD() {
-		setTitle("BOMBERMAN"); // Fenstertitel
-		setSize(500, 500); // Fenstergröße
-		setVisible(true); // Sichtbarkeit auf true setzen
-		
-		JLabel inhalt = new JLabel();
-		inhalt.setText("Hier kommt der Inhalt hin");
-	
-		add(inhalt); // hinzufügen von inhalt zum Fenster
-	}
+    public static void main(String[] args) {
+    	new SPIELFELD();
+    }
+    
+        public SPIELFELD() {
+        	JPanel panel = new JPanel(new GridLayout(10, 10, 0, 0)); // anlegen vom Gridlayout
+
+            for (int i = 0; i < 100; i++) {
+                JLabel label = new JLabel(new ImageIcon("images/bild.png"), JLabel.CENTER); // Bild wird als Array angezeigt
+                panel.add(label); // hinzufügen vom Gridlayout
+            }
+                
+                setContentPane(panel); // hinzufügen der Bilder
+                setTitle("BOMBERMAN"); // Fenstertitel
+                setSize(500, 500); // Fenstergröße
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setVisible(true); // Sichtbarkeit auf true setzen
+        }
 }
