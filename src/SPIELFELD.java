3c88f051c7e256e3c3fdbcda9b05dc368ae41c05
Christopher Vogt <christopher.vogt@uni-duesseldorf.de>
2012-05-20 22:01:33 +0200

    sauber programmiertes leeres JFrame

--- src/SPIELFELD.java
+++ src/SPIELFELD.java
@@ -2,17 +2,19 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 
-public class SPIELFELD {
+public class SPIELFELD extends JFrame{
 	public static void main(String[] args) {
+		new SPIELFELD();
+	}		
 		
-		JFrame window = new JFrame(); // Erstellung eines Fensters
-		window.setTitle("BOMBERMAN"); // Fenstertitel
-		window.setSize(500, 500); // Fenstergröße
-		window.setVisible(true); // Sichtbarkeit auf true setzen
+	public SPIELFELD() {
+		setTitle("BOMBERMAN"); // Fenstertitel
+		setSize(500, 500); // Fenstergröße
+		setVisible(true); // Sichtbarkeit auf true setzen
 		
 		JLabel inhalt = new JLabel();
 		inhalt.setText("Hier kommt der Inhalt hin");
 	
-		window.add(inhalt); // hinzufügen von inhalt zum Fenster
+		add(inhalt); // hinzufügen von inhalt zum Fenster
 	}
 }
