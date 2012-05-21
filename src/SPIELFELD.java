79bd4a5b82d189e10bb07b3f26ba978102d7afbb
Nils Frommelius <Nils.Frommelius@uni-duesseldorf.de>
2012-05-21 14:38:29 +0200

    Bild2

--- src/SPIELFELD.java
+++ src/SPIELFELD.java
@@ -10,13 +10,13 @@ public class SPIELFELD extends JFrame {
         	JPanel panel = new JPanel(new GridLayout(10, 10, 0, 0)); // anlegen vom Gridlayout
 
             for (int i = 0; i < 100; i++) {
-                JLabel label = new JLabel(new ImageIcon("images/bild.png"), JLabel.CENTER); // Darstellung der Bilder
-                panel.add(label); // einfÃ¼gen der Bilder in das Gridlayout
+                JLabel label = new JLabel(new ImageIcon("images/bild2.png"), JLabel.CENTER); // Darstellung der Bilder
+                panel.add(label); // einfügen der Bilder in das Gridlayout
             }
                 
-                setContentPane(panel); // hinzufÃ¼gen des fertigen Gridlayout- + Bilderkonstruktes
+                setContentPane(panel); // hinzufügen des fertigen Gridlayout- + Bilderkonstruktes
                 setTitle("BOMBERMAN"); // Fenstertitel
-                setSize(500, 500); // FenstergrÃ¶ÃŸe
+                setSize(500, 500); // Fenstergröße
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 setVisible(true); // Sichtbarkeit auf true setzen
         }
