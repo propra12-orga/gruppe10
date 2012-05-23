64e41f353bfb8a4ae557ef71234f63d5a2637a8e
Christopher Vogt <christopher.vogt@uni-duesseldorf.de>
2012-05-23 02:35:00 +0200

    Grid erweitert auf 11. (9 für das Spielfeld und 2 für die aeusseren Waende

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -3,21 +3,24 @@ import javax.swing.*;
 
 public class Spielfeld extends JFrame {
     public static void main(String[] args) {
+    	
+    	BomberManMove bomberManMove = new BomberManMove();
+    	bomberManMove.bomberManMove();
+    	
     	new Spielfeld();
     }
     
         public Spielfeld() {
-        	JPanel panel = new JPanel(new GridLayout(10, 10, 0, 0)); // anlegen vom Gridlayout
+        	JPanel panel = new JPanel(new GridLayout(11, 11, 0, 0)); // anlegen vom Gridlayout
 
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 121; i++) {
                 JLabel label = new JLabel(new ImageIcon("images/bild2.png"), JLabel.CENTER); // Darstellung der Bilder
                 panel.add(label); // einfuegen der Bilder in das Gridlayout
             }
                 
-            	new BomberManMove();
                 setContentPane(panel); // hinzufuegen des fertigen Gridlayout- + Bilderkonstruktes
                 setTitle("BOMBERMAN"); // Fenstertitel
-                setSize(500, 500); // Fenstergroesse
+                setSize(550, 550); // Fenstergroesse
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 setVisible(true); // Sichtbarkeit auf true setzen
         }
