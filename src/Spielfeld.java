5eda138cb5e425e5c17b2adcd755043b42f649b6
Christopher Vogt <christopher.vogt@uni-duesseldorf.de>
2012-05-27 18:31:06 +0200

    JLabel Array statt Bilderarray + Bomberwoman kann sich jetzt bewegen

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -10,11 +10,13 @@ public class Spielfeld extends JFrame implements KeyListener {
     	new Spielfeld();
     }
 	
+    int zeile = 1, spalte = 1;
+    JPanel panel = new JPanel(new GridLayout(11, 11, 0, 0)); // anlegen vom Gridlayout
+    JLabel feld[][] = new JLabel[11][11]; // Darstellung der Bilder
     
         public Spielfeld() {
         	
-        	JPanel panel = new JPanel(new GridLayout(11, 11, 0, 0)); // anlegen vom Gridlayout
-        	
+
         	setContentPane(panel); // hinzufuegen des fertigen Gridlayout- + Bilderkonstruktes
             setTitle("BOMBERMAN"); // Fenstertitel
             setSize(550, 550); // Fenstergroesse
@@ -24,25 +26,23 @@ public class Spielfeld extends JFrame implements KeyListener {
             
             addKeyListener(this); // einfuegen des KeyListeners
             
-            ImageIcon feld[][] = new ImageIcon[11][11]; // Deklarieren eines Bilderarrays
-        	JLabel label = new JLabel(); // Darstellung der Bilder
         	
             	for (int x=0; x<11; x++) { // zweidimensionale Schleife
     				for (int y=0; y<11; y++) {
     					 if (x == 0 || x == 10 || y == 0 || y == 10 || (x%2 == 0 && y%2 == 0)) { // Koordinaten für die Wände
-                             feld[x][y] = new ImageIcon("images/bild2.png"); // Feld an der Stelle x, y bekommt dieses Bild zugewiesen
-                             label = new JLabel(feld[x][y]); // hinzufuegen zum JLabel
-                             panel.add(label); // einfuegen des JLabels
+                             feld[x][y] = new JLabel (new ImageIcon("images/bild2.png")); // Feld an der Stelle x, y bekommt dieses Bild zugewiesen
+                             panel.add(feld[x][y]); // einfuegen des JLabels
                              }
     					 else { // ansonten Rasen
-    						 feld[x][y] = new ImageIcon("images/gras.png");
-                             label = new JLabel(feld[x][y]);
-                             panel.add(label);
+    						 feld[x][y] = new JLabel (new ImageIcon("images/gras.png")); // Feld an der Stelle x, y bekommt dieses Bild zugewiesen
+    						 panel.add(feld[x][y]); // einfuegen des JLabels
+                            
     					 }
     				}
-    			
             	}
+            feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwoman.png")); // Spielfigur
         }
+        
         
         // KeyListener
         public boolean up, down, left, right;
@@ -52,38 +52,36 @@ public class Spielfeld extends JFrame implements KeyListener {
 			if(key == KeyEvent.VK_LEFT) {
 				left = true;
 				System.out.println("links");
+				spalte--;
+				feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwomanleft.png"));
+				validate();
 			}
 			else if (key == KeyEvent.VK_RIGHT) { // else if, damit das Programm nicht jede if Bedingung einzeln durchgehen muss
 				right = true;
 				System.out.println("rechts");
+				spalte++; // spalte wird um 1 erhöht
+				feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwomanright.png"));
 			}
 			else if (key == KeyEvent.VK_UP) {
 				up = true;
 				System.out.println("hoch");
+				zeile--; // zeile wird um 1 verringert
+				feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwomanback.png"));
 			}
 			else if (key == KeyEvent.VK_DOWN) {
 				down = true;
 				System.out.println("runter");
+				zeile++;
+				feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwoman.png"));
 			}
 			else {
 				System.out.println("andere Taste");
 			}
 		}
-		
+
+
 		public void keyReleased(KeyEvent e) {
-			int key = e.getKeyCode();
-			if(key == KeyEvent.VK_LEFT) {
-				left = false;
-			}
-			else if (key == KeyEvent.VK_RIGHT) {
-				right = false;
-			}
-			else if (key == KeyEvent.VK_UP) {
-				up = false;
-			}
-			else if (key == KeyEvent.VK_DOWN) {
-				down = false;
-			}
+
 		}
 
 		public void keyTyped(KeyEvent e) { // muss implementiert sein, kann aber leer bleiben, da keine Verwendung besteht
