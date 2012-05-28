408b02f1f24dd8a82c06b0866a8d5ac1cc961c78
Christopher Vogt <christopher.vogt@uni-duesseldorf.de>
2012-05-29 00:12:28 +0200

    Kommentar update

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -13,12 +13,26 @@ public class Spielfeld extends JFrame implements KeyListener {
     Boolean laufen = true;
     int zeile = 1, spalte = 1;
     JPanel panel = new JPanel(new GridLayout(11, 11, 0, 0)); // anlegen vom Gridlayout
-    JLabel feld[][] = new JLabel[11][11]; // Darstellung der Bilder
+    JLabel feld[][] = new JLabel[11][11]; // 11 mal 11 JLabel
     
         public Spielfeld() {
         	
-
-        	setContentPane(panel); // hinzufuegen des fertigen Gridlayout- + Bilderkonstruktes
+            	for (int x=0; x<11; x++) { // zweidimensionale Schleife
+    				for (int y=0; y<11; y++) {
+    					 if (x == 0 || x == 10 || y == 0 || y == 10 || (x%2 == 0 && y%2 == 0)) { // Koordinaten für die Wände
+                             feld[x][y] = new JLabel (new ImageIcon("images/bild2.png")); // feld (JLabel) an der Stelle x, y bekommt dieses Bild zugewiesen
+                             panel.add(feld[x][y]); // einfuegen von JLabel in das JPanel
+                             }
+    					 else { // ansonten Rasen
+    						 feld[x][y] = new JLabel (new ImageIcon("images/gras.png"));
+    						 panel.add(feld[x][y]);
+    					 }
+    				}
+            	}
+            	
+            feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwoman.png")); // Spielfigur
+            
+            setContentPane(panel); // hinzufuegen des fertigen Gridlayout- + Bilderkonstruktes
             setTitle("BOMBERWOMAN"); // Fenstertitel
             setSize(550, 550); // Fenstergroesse
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,22 +40,6 @@ public class Spielfeld extends JFrame implements KeyListener {
             setVisible(true); // Sichtbarkeit auf true setzen
             
             addKeyListener(this); // einfuegen des KeyListeners
-            
-        	
-            	for (int x=0; x<11; x++) { // zweidimensionale Schleife
-    				for (int y=0; y<11; y++) {
-    					 if (x == 0 || x == 10 || y == 0 || y == 10 || (x%2 == 0 && y%2 == 0)) { // Koordinaten für die Wände
-                             feld[x][y] = new JLabel (new ImageIcon("images/bild2.png")); // Feld an der Stelle x, y bekommt dieses Bild zugewiesen
-                             panel.add(feld[x][y]); // einfuegen des JLabels
-                             }
-    					 else { // ansonten Rasen
-    						 feld[x][y] = new JLabel (new ImageIcon("images/gras.png")); // Feld an der Stelle x, y bekommt dieses Bild zugewiesen
-    						 panel.add(feld[x][y]); // einfuegen des JLabels
-                            
-    					 }
-    				}
-            	}
-            feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwoman.png")); // Spielfigur
         }
         
         
@@ -49,6 +47,7 @@ public class Spielfeld extends JFrame implements KeyListener {
         public boolean up, down, left, right;
 	    
 	    public void keyPressed(KeyEvent e) {
+	    	
 			int key = e.getKeyCode();
 			if (key == KeyEvent.VK_LEFT && laufen == true) {
 				left = true;
@@ -56,7 +55,6 @@ public class Spielfeld extends JFrame implements KeyListener {
 				feld[zeile][spalte].setIcon(new ImageIcon("images/gras.png")); // Gras an die Stelle, von der Bomberwoman kommt
 				spalte--;
 				feld[zeile][spalte].setIcon(new ImageIcon("images/Bomberwomanleft.png"));
-				validate();
 			}
 			else if (key == KeyEvent.VK_RIGHT && laufen == true) { // else if, damit das Programm nicht jede if Bedingung einzeln durchgehen muss
 				right = true;
@@ -85,11 +83,11 @@ public class Spielfeld extends JFrame implements KeyListener {
 		}
 
 
-		public void keyReleased(KeyEvent e) {
+		public void keyReleased(KeyEvent e) { // muss implementiert sein, kann aber leer bleiben, da keine Verwendung besteht
 
 		}
 
-		public void keyTyped(KeyEvent e) { // muss implementiert sein, kann aber leer bleiben, da keine Verwendung besteht
+		public void keyTyped(KeyEvent e) {
 			
 		}
 }
