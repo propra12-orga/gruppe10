a1b325bcad07c8fd2f4aee9d7ab5ab12887f1a19
Andreas Lisowsky <andreas.lisowsky@uni-duesseldorf.de>
2012-06-07 18:40:17 +0200

    Bombe kommt

---
+++ src/Bombe.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Bombe {                                    //Ganz billige Bombe, damit ihr schonmal Objekte davon erstellen könnt.
+
+	private int xPos, yPos, rad;                    //Die Parameter von der Bombe sind momentan: Radius, und die Positionen auf der x- und y-Achse.
+
+	public Bombe(int xPos, int yPos, int rad) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.rad = rad;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
