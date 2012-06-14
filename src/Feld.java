3b194606235cfc0e20146c7866275f04d66556a2
Christopher Vogt <christopher.vogt@uni-duesseldorf.de>
2012-06-14 18:10:40 +0200

    Kommentar update 2

--- src/Feld.java
+++ src/Feld.java
@@ -1,7 +1,8 @@
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class Feld extends JLabel {
+public class Feld extends JLabel { // Feld ist ein JLabel
+	// deklarieren von Feldeigenschaften
 	public boolean laufen = false, endpunkt = false, zerbrechen = false, spieler1 = false, spieler2 = false, wand = false, bombe = false, explosion = false;
 
 	public Feld(ImageIcon imageIcon) {
--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -5,19 +5,19 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Spielfeld implements KeyListener {
-	static int zeile = 1, spalte = 1, zeile2 = 9, spalte2 = 9;
-	static JFrame frame = new JFrame();
-	static JPanel panel = new JPanel(new GridLayout(11, 11, 0, 0)); 
-	public static Feld[][] feld = new Feld[11][11]; 
+public class Spielfeld implements KeyListener { // Implementierung vom Keylistener
+	static int zeile = 1, spalte = 1, zeile2 = 9, spalte2 = 9; // deklarieren der Startposition der Bomberwomen
+	static JFrame frame = new JFrame(); // erstellen eines neuen JFrame mit dem Namen "frame"
+	static JPanel panel = new JPanel(new GridLayout(11, 11, 0, 0)); // erstellen eines neuen JPanel mit dem Namen "panel" (besitzt ein 11 mal 11 Gridlayout)
+	public static Feld[][] feld = new Feld[11][11]; // erstellen eines neuen 11 mal 11 Feld Arrays mit dem Namen "feld" (Feld Array = JLabel Array)
 	
 	public Spielfeld() {
-		for(int x = 0; x < 11; x++) { 
+		for(int x = 0; x < 11; x++) { // 2-dimensionale for-Schleife zur Erstellung des Spielfeldes
 			for(int y = 0; y < 11; y++) {
-				if(x == 0 || x == 10 || y == 0 || y == 10 || (x % 2 == 0 && y % 2 == 0)) { 
-					Spielfeld.feld[x][y] = new Feld(new ImageIcon("images/wand2.png"));
-					Blockeigenschaft.wand(x, y);
-					panel.add(feld[x][y]); 
+				if(x == 0 || x == 10 || y == 0 || y == 10 || (x % 2 == 0 && y % 2 == 0)) { // Koordinaten fuer die nicht zerstoerbaren Waende
+					Spielfeld.feld[x][y] = new Feld(new ImageIcon("images/wand2.png")); // setzte eine Wand and die Stelle x, y
+					Blockeigenschaft.wand(x, y); // weise der Wand diese Eigenschaften zu (siehe Klasse Blockeigenschaften -> wand)
+					panel.add(feld[x][y]); // Füge das fertige JLabel in das JPanel ein (feld -> panel)
 
 				}
 				else if((x == 3 && y == 2) || (x == 2 && y == 5) || (x == 2 && y == 7) || (x == 2 && y == 3) || (x == 3 && y == 3) || (x == 3 && y == 4)
@@ -30,30 +30,30 @@ public class Spielfeld implements KeyListener {
 					Blockeigenschaft.broecklig(x, y);
 					panel.add(feld[x][y]);
 				}
-				else { 
+				else { // ansonsten setzte Rasen
 					feld[x][y] = new Feld(new ImageIcon("images/rasen.png"));
 					Blockeigenschaft.rasen(x, y);
 					panel.add(feld[x][y]);
 				}
 			}
 		}
-		Blockeigenschaft.spieler1(zeile, spalte, "down");
-		Blockeigenschaft.spieler2(zeile2, spalte2, "up");
-		Blockeigenschaft.endpunkt(5, 5);
+		Blockeigenschaft.spieler1(zeile, spalte, "down"); // Startposition von Spieler 1 & zuweisen von Eigenschaften
+		Blockeigenschaft.spieler2(zeile2, spalte2, "up"); // Startposition von Spieler 2 & zuweisen von Eigenschaften
+		Blockeigenschaft.endpunkt(5, 5); // setzte den Endpunkt in die Mitte (5, 5)
 
-		frame.add(panel);
-		frame.setTitle("BOMBERWOMAN");
-		frame.setSize(550, 550);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false); 
-		frame.setVisible(true); 
-		frame.addKeyListener(this); 
+		frame.add(panel); // fuege das JPanel in das JFrame (panel -> frame)
+		frame.setTitle("BOMBERWOMAN"); // setzte Fenstertitel
+		frame.setSize(550, 550); // setzte Fenstergroesse
+		frame.setLocationRelativeTo(null); // zentriere das Fenster
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // setze den Exit Button (X) auf "EXIT_ON_CLOSE" um das Programm ordentlich zu beenden
+		frame.setResizable(false); // ausschalten vom maximieren des Fensters
+		frame.setVisible(true); // zeige das Fenster an
+		frame.addKeyListener(this); // fuege den Keylistener in das JFrame ein
 	}
 
-	public void keyPressed(KeyEvent e) {
-		Move move = new Move();
-		move.keyboard(e);
+	public void keyPressed(KeyEvent e) { // wenn eine Taste gedrueckt wird
+		Move move = new Move(); // deklarieren eines neuen Objektes "move" von der Klasse "Move"
+		move.keyboard(e); // fuehre die Methode "keyboard" aus
 		Move2 move2 = new Move2();
 		move2.keyboard(e);
 	}
